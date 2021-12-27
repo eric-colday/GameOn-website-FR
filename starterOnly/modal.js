4af6370a68dc16f5b1fd3e92651546b975d9d3b9
Eric Noubissi <noubissi10@yahoo.fr>
2021-12-27 20:53:29 +0100

    Test commit

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,3 +1,48 @@
+/** VARIABLES
+ * ---------------------------------------
+ * ---------------------------------------
+*/
+
+/**  DOM Elements */
+const modalbg = document.querySelector(".bground");
+const modalBtn = document.querySelectorAll(".modal-btn");
+const formData = document.querySelectorAll(".formData");
+// Fermer le bouton (x)
+const closeModalBtn = document.querySelectorAll("#close");
+// Message de succès
+const successMessageElt = document.querySelectorAll("#sucess-message");
+// Mise à jour id="form"
+const formElt = document.querySelectorAll("#form");
+// Fermer id="sucess-close-btn"
+const successCloseBtnElt = document.querySelectorAll("#sucess-close-btn");
+
+/** Form elements */ 
+const firstElt = document.getElementById("first");
+const lastElt = document.getElementById("last");
+const emailElt = document.getElementById("email");
+const birthdateElt = document.getElementById("birthdate");
+const quantityElt = document.getElementById("quantity");
+const cityElt = document.getElementById("input[type=radio]");
+
+/** EVENTS
+ * ---------------------------------------
+ * ---------------------------------------
+*/
+
+/* launch modal event */
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+/* close modal events: CLICK  */
+closeModalBtn.forEach(elt => elt.addEventListener("click", closeModal));
+
+/*  */
+
+/** FUNCTIONS
+ * ---------------------------------------
+ * ---------------------------------------
+*/ 
+
+/** Manage nav button on responsive */
 function editNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -7,17 +52,20 @@ function editNav() {
   }
 }
 
-// DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
 
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
-// launch modal form
+/* launch modal form */
 function launchModal() {
-  modalbg.style.display = "block";
+  modalbg.style.display = "block";  
 }
+
+/* close modal form */
+function closeModal() {
+  modalbg.style.display = "none";
+}
+
+
+
+
+
 
 
